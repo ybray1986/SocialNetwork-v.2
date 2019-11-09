d5697e43c54e5028f01e13968e94d8d580f4f268
Аман Ыбрай <ybray1986@gmail.com>
2019-11-09 17:28:25 +0600

    *

--- SocialNetwork/SocialNetwork.WEB/App_Start/BundleConfig.cs
+++ SocialNetwork/SocialNetwork.WEB/App_Start/BundleConfig.cs
@@ -20,8 +20,8 @@ namespace SocialNetwork.WEB
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/app.css",
+                      "~/Content/theme.css"));
         }
     }
 }
---
+++ SocialNetwork/SocialNetwork.WEB/Controllers/PostController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocialNetwork.WEB.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        public PartialViewResult Post()
+        {
+            return PartialView();
+        }
+    }
+}
